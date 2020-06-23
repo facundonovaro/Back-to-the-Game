e41931d76fa1d2ae83fb36dde878a6ace7d1d8a5
FlaviaCohen <flaviacohen1988@gmail.com>
2020-06-23 14:13:41 -0300

    product Routes y Controllers + SingleProduct Component y Container

--- front/src/store/constants.js
+++ front/src/store/constants.js
@@ -3,5 +3,4 @@ export const LOGIN_USER = 'LOGIN_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
 
 // Single Product
-
 export const GET_PRODUCT = 'GET_PRODUCT';
--- front/src/store/reducers/index.js
+++ front/src/store/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-
-
+import { productReducer } from './product-reducer';
 
 export default combineReducers({
-    
-
+    productReducer,
 });
