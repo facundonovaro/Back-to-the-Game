8a34ab3c158541d65901311cd0170783c81da5db
cristianlunachavarro <cristianlunachavarro@gmail.com>
2020-06-23 14:23:12 -0300

    working progress on Products back and front

--- front/src/store/constants.js
+++ front/src/store/constants.js
@@ -1,7 +1,12 @@
 // Users
 export const LOGIN_USER = 'LOGIN_USER';
+
 export const LOGOUT_USER = 'LOGOUT_USER';
 
-// Single Product
+// Products
 
+export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS' 
+
+//Single Produc
 export const GET_PRODUCT = 'GET_PRODUCT';
+
--- front/src/store/reducers/index.js
+++ front/src/store/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
-
+import productsReducer from './products'
 
 
 export default combineReducers({
-    
-
+    productsReducer,
 });
