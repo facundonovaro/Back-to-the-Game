23f07b43f7cdaa7aeb0ec0f60b332c698ef22ee1
FlaviaCohen <flaviacohen1988@gmail.com>
2020-06-23 17:59:07 -0300

    antes de pullear de joaco

--- back/controllers/productControllers.js
+++ back/controllers/productControllers.js
@@ -1,17 +1,9 @@
 const { Product } = require('../models')
 
-<<<<<<< HEAD
 const findProduct = (req, res) => {
-    Product.findByPk({
-        where: {
-            id: req.params.id
-        }
-    })
+    Product.findByPk(req.params.id)
     .then(product => res.send(product))
 }
-
-module.exports = findProduct
-=======
 
 const findAllProducts = (req, res) =>{
     Product.findAll()
@@ -20,5 +12,5 @@ const findAllProducts = (req, res) =>{
     })
 }
 
-module.exports = findAllProducts
->>>>>>> eeb82908ed455e43e3337e034afd4ad5ac651f45
+module.exports = { findAllProducts, findProduct }
+
--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -3,11 +3,12 @@ import LoginContainer from '../containers/LoginContainer'
 import ProductsContainer from "../containers/ProductsContainer"
 import RegisterContainer from "../containers/RegisterContainer"
 import { Switch, Route, Redirect } from "react-router-dom";
+import SingleProductContainer from '../containers/SingleProductContainer';
 
 export default () => (
   <div id="main" className="container-fluid">
     <Switch>
-      <Route path='/products' component={ProductsContainer}/>
+      <Route exact path='/products' component={ProductsContainer}/>
       <Route path="/users/register" component={RegisterContainer} />
       <Route path="/users/login" component={LoginContainer} />
       <Route path='/products/:id' component={SingleProductContainer}/>
