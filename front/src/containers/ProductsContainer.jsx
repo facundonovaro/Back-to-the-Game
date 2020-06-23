137b0fdd4cdf25b870261f852285550f1520a35c
cristianlunachavarro <cristianlunachavarro@gmail.com>
2020-06-23 16:52:00 -0300

    Commiting to get the merge process donde

--- front/src/containers/ProductsContainer.jsx
+++ front/src/containers/ProductsContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import Products  from '../components/Products'
-import fetchProducts from '../store/actions/products'
+import {fetchProducts} from '../store/actions/products'
 
 class ProductsContainer extends React.Component{
 
