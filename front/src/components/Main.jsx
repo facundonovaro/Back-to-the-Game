ac97f35fe644079aa539df5d87f3958d0d5900f2
dennismromeroa <dennisrome1987@gmail.com>
2020-06-24 14:16:37 -0300

    primer commit single user

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -4,15 +4,17 @@ import ProductsContainer from "../containers/ProductsContainer"
 import RegisterContainer from "../containers/RegisterContainer"
 import SingleProductContainer from "../containers/SingleProductContainer";
 import { Switch, Route, Redirect } from "react-router-dom";
+import SingleuserContainer from "../containers/SingleuserContainer";
 
 
 export default () => (
   <div id="main" className="container-fluid">
     <Switch>
-      <Route exact path='/products' component={ProductsContainer}/>
+      <Route exact path='/products' component={ProductsContainer} />
       <Route path="/users/register" component={RegisterContainer} />
       <Route path="/users/login" component={LoginContainer} />
-      <Route path='/products/:id' component={SingleProductContainer}/>
+      <Route path="/users/:id" component={SingleuserContainer} />
+      <Route path='/products/:id' component={SingleProductContainer} />
       {/* <Redirect to="/"></Redirect> */}
     </Switch>
   </div>
