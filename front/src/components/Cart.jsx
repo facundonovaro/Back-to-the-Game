a90e94f94b5439e469fe8b673ffd35844fba9910
facundonovaro <facunovaroh@gmail.com>
2020-06-25 11:45:49 -0300

    cart done

--- front/src/components/Cart.jsx
+++ front/src/components/Cart.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { Alert, ListGroup, Button } from "react-bootstrap";
 
-const Cart = ({ deleteCart, updateCart, cart, user }) => {
+const Cart = ({ deleteCart, updateCart, cart, user, totalQuantity }) => {
   return (
     <div>
       <h1>Tu Carrito</h1>
       {cart &&
         cart.map((product) => (
           <div key={product.id}>
-            <h2>{product.product.name}</h2>
-            <button
+            <ListGroup>
+              <ListGroup.Item>{product.product.name}</ListGroup.Item>
+              <ListGroup.Item>
+                Precio individual - ${product.product.price}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Sub Total - ${product.quantity * product.product.price}
+              </ListGroup.Item>
+              <ListGroup.Item>{product.quantity}</ListGroup.Item>
+            </ListGroup>
+            <Button
+              variant="dark"
               onClick={() => {
                 updateCart({
                   quantity: product.quantity + 1,
@@ -16,11 +27,16 @@ const Cart = ({ deleteCart, updateCart, cart, user }) => {
                   userId: user.id,
                 });
               }}
+              disabled={product.quantity >= product.product.stock}
             >
               + 1
-            </button>
-            <h2>{product.quantity}</h2>
-            <button
+            </Button>
+            {product.quantity >= product.product.stock ? (
+              <Alert variant="primary">Llegaste al máximo stock</Alert>
+            ) : null}
+
+            <Button
+              variant="dark"
               onClick={() => {
                 updateCart({
                   quantity: product.quantity - 1,
@@ -28,18 +44,29 @@ const Cart = ({ deleteCart, updateCart, cart, user }) => {
                   userId: user.id,
                 });
               }}
+              disabled={product.quantity <= 1}
             >
               - 1
-            </button>
-            <button
+            </Button>
+            {product.quantity <= 1 ? (
+              <Alert variant="primary">
+                Debes tener al menos uno. Si lo quieres sacar, presionar
+                "Delete"
+              </Alert>
+            ) : null}
+            <Button
+              variant="dark"
               onClick={() => {
                 deleteCart(product.id, user.id);
               }}
             >
               Delete Product
-            </button>
+            </Button>
           </div>
         ))}
+      <ListGroup>
+        <ListGroup.Item>Total del Carrito - ${totalQuantity}</ListGroup.Item>
+      </ListGroup>
     </div>
   );
 };
